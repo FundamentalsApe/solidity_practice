127f0bed366d891be1b87888672dc7c5da55d977
Vaibhav Sharma <vaibhav.sharma@soroco.com>
2022-01-23 12:27:18 +0530

    added token contract for FAT

---
+++ fat_token_contract.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.6.0;
+
+contract MyToken{
+    uint32 maxLimit = 10000000;
+    string tokenName = "ApeToken";
+    struct token{
+        uint32 decimals;
+        string name;
+    }
+    mapping(address => uint256) ownerBalance;  // Storing how much FAT you have
+    mapping(address => address[]) ownerApprovedAddress; // Storing who can transfer your tokens for you
+
+    function _transfer(address _from, address _to, uint amount) internal{
+        bool approved = false;
+        for(uint i; i<ownerApprovedAddress[_from].length;i++){
+            if(msg.sender == ownerApprovedAddress[_from][i]){
+                approved = true;
+            }
+        }
+        // Check that only owner or approved address can move a fund and that the address has enough $FAT
+        require((msg.sender == _from || approved) && ownerBalance[_from] >= amount,"balance too low or you are not approved to transfer tokens");
+        ownerBalance[_from] -= amount;
+        ownerBalance[_to] += amount;
+    }
+
+    function approve(address _from, address _to) external{
+        require(msg.sender == _from, "Only owner can approve another address to transfer tokens");
+        ownerApprovedAddress[_from].push(_to);
+    }
+
+    function balanceOf(address owner) public view returns(uint) {
+        return ownerBalance[owner];
+    }
+}
