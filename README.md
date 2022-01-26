ca0daf5b9639c613be009eb11875437ecb7e3015
Vaibhav Sharma <vaibhav.sharma@soroco.com>
2022-01-26 16:47:48 +0530

    published part 2 of ERC-20 meditations and updated token contract

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # solidity_practice
+
+- Contains contracts outlined in articles for [this](https://medium.com/@FundamentalsApe) medium account.
--- fat_token_contract.sol
+++ fat_token_contract.sol
@@ -1,34 +1,116 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.0;
 
-contract MyToken{
-    uint32 maxLimit = 10000000;
-    string tokenName = "ApeToken";
-    struct token{
-        uint32 decimals;
-        string name;
-    }
+// SPDX-License-Identifier: MIT
+
+import "./SafeMath.sol";
+
+/**
+    @title ERC-20 token implementation
+    @notice Based on the ERC-20 token standard as defined at
+            https://eips.ethereum.org/EIPS/eip-20
+ */
+contract FAToken {
+
+    using SafeMath for uint256;
+
+    string public symbol = "FAT";
+    string public name = "Fundamentals Ape Token";
+    uint256 public decimals = "18";
+    uint256 public totalSupply = 1000000;
+
     mapping(address => uint256) ownerBalance;  // Storing how much FAT you have
-    mapping(address => address[]) ownerApprovedAddress; // Storing who can transfer your tokens for you
+    mapping(address => mapping(address => uint256)) allowed; // Storing who can transfer your tokens for you
 
-    function _transfer(address _from, address _to, uint amount) internal{
-        bool approved = false;
-        for(uint i; i<ownerApprovedAddress[_from].length;i++){
-            if(msg.sender == ownerApprovedAddress[_from][i]){
-                approved = true;
-            }
-        }
-        // Check that only owner or approved address can move a fund and that the address has enough $FAT
-        require((msg.sender == _from || approved) && ownerBalance[_from] >= amount,"balance too low or you are not approved to transfer tokens");
-        ownerBalance[_from] -= amount;
-        ownerBalance[_to] += amount;
+    event Transfer(address from, address to, uint256 value);
+    event Approval(address owner, address spender, uint256 value);
+
+    constructor()
+        public
+    {
+        ownerBalance[msg.sender] = _totalSupply;
+        emit Transfer(address(0), msg.sender, _totalSupply);
     }
 
-    function approve(address _from, address _to) external{
-        require(msg.sender == _from, "Only owner can approve another address to transfer tokens");
-        ownerApprovedAddress[_from].push(_to);
+    /**
+        @notice Getter to check the current balance of an address
+        @param _owner Address to query the balance of
+        @return Token balance
+     */
+    function balanceOf(address _owner) public view returns (uint256) {
+        return ownerBalance[_owner];
     }
 
-    function balanceOf(address owner) public view returns(uint) {
-        return ownerBalance[owner];
+    /**
+        @notice Getter to check the amount of tokens that an owner allowed to a spender
+        @param _owner The address which owns the funds
+        @param _spender The address which will spend the funds
+        @return The amount of tokens still available for the spender
+     */
+    function allowance(
+        address _owner,
+        address _spender
+    )
+        public
+        view
+        returns (uint256)
+    {
+        return allowed[_owner][_spender];
     }
+
+    /**
+        @notice Approve an address to spend the specified amount of tokens on behalf of msg.sender
+        @dev Beware that changing an allowance with this method brings the risk that someone may use both the old
+             and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+             race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+             https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+        @param _spender The address which will spend the funds.
+        @param _value The amount of tokens to be spent.
+        @return Success boolean
+     */
+    function approve(address _spender, uint256 _value) public returns (bool) {
+        allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    /** shared logic for transfer and transferFrom */
+    function _transfer(address _from, address _to, uint256 _value) internal {
+        require(ownerBalance[_from] >= _value, "Insufficient balance");
+        ownerBalance[_from] = ownerBalance[_from].sub(_value);
+        ownerBalance[_to] = ownerBalance[_to].add(_value);
+        emit Transfer(_from, _to, _value);
+    }
+
+    /**
+        @notice Transfer tokens to a specified address
+        @param _to The address to transfer to
+        @param _value The amount to be transferred
+        @return Success boolean
+     */
+    function transfer(address _to, uint256 _value) public returns (bool) {
+        _transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    /**
+        @notice Transfer tokens from one address to another
+        @param _from The address which you want to send tokens from
+        @param _to The address which you want to transfer to
+        @param _value The amount of tokens to be transferred
+        @return Success boolean
+     */
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    )
+        public
+        returns (bool)
+    {
+        require(allowed[_from][msg.sender] >= _value, "Insufficient allowance");
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+        _transfer(_from, _to, _value);
+        return true;
+    }
+
 }
